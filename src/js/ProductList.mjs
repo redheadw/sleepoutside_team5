9ef4ecc39e9d6010b9fe255f007a5282f0dab825
jazminruiz16 <jazmingruiz@gmail.com>
2026-03-16 22:17:19 -0400

    week 2

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,31 @@
+import { renderListWithTemplate } from "./utils.mjs";
+function productCardTemplate(product) {
+    return `<li class="product-card">
+        <a href="product_pages/?product=">
+            <img src="${product.Image}" alt="Image of ${product.Name}">
+            <h2 class="card__brand">${product.Brand.Name}</h2>
+            <h3 class="card__name">${product.Name}</h3>
+            <p class="product-card__price">$${product.FinalPrice}</p>
+        </a>
+    </li>`
+}
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+    async init() {
+        // the dataSource will return a Promise...
+        const list = await this.dataSource.getData();
+        this.renderList(list);
+       
+    }
+    renderList(list) {
+        //const htmlStrings = list.map(productCardTemplate);
+        //this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
+
+    }
+
+}
