0b8ad99f736116253c4196b63a813e1220612e0c
Ethan O'Brien <ethan.a.obrien@gmail.com>
2026-03-25 22:24:39 -0500

    Add css and final checkout activity

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -12,6 +12,7 @@ export default class CheckoutProcess {
     }
     async init() {
         this.list = await getCartContents(this.key);
+        //console.log(this.list);
     }
     calculateItemSummary() {
         this.calculateItemSubTotal();
@@ -43,5 +44,60 @@ export default class CheckoutProcess {
         document.querySelector(`${this.outputSelector} #order-tax`).textContent = `$${this.tax.toFixed(2)}`;
         document.querySelector(`${this.outputSelector} #order-shipping`).textContent = `$${this.shipping.toFixed(2)}`;
         document.querySelector(`${this.outputSelector} #order-total`).textContent = `$${this.orderTotal.toFixed(2)}`;
+    }
+
+    packageItems(items) {
+        return items.map(item => ({
+            id: item.product.Id,
+            name: item.product.Name,
+            price: item.product.FinalPrice,
+            quantity: item.cartItem.count
+        }));
+    }
+    formDataToJSON(formElement) {
+        const formData = new FormData(formElement),
+        convertedJSON = {};
+        formData.forEach(function (value, key) {
+            convertedJSON[key] = value;
+        });
+        return convertedJSON;
+    }
+    async checkout(form) {
+        const formData = this.formDataToJSON(form);
+
+        const items = this.packageItems(await getCartContents(this.key));
+        const orderData = {
+            orderDate: new Date().toISOString(),
+            fname: formData.fname,
+            lname: formData.lname,
+            street: formData.street,
+            city: formData.city,
+            state: formData.state,
+            zip: formData.zip,
+            cardNumber: "1234123412341234",
+            expiration: formData.expiration,
+            code: "123",
+            items: items,
+            orderTotal: this.orderTotal.toFixed(2),
+            shipping: this.shipping,
+            tax: this.tax.toFixed(2)
+        };
+
+        const url = "/checkout";
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(orderData)
+        };
+
+        try {
+            const response = await fetch(url, options);
+            return response;
+        } catch (error) {
+            console.error("Error during checkout:", error);
+            throw error;
+        }
     }
 }
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -7,3 +7,15 @@ const process = new CheckoutProcess();
 await process.init();
 
 process.displaySubtotal("so-cart", ".order-summary");
+document.querySelector("form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    try {
+        const response = await process.checkout(form);
+
+        console.log('Checkout response:', response);
+
+    } catch (error) {
+        console.error('Checkout error:', error);
+    }
+});
